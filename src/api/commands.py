3c7346245c4fd07c515818e35c2f78a9ef60ac24
jgonzalez-89 <kbcerverjo@gmail.com>
2023-02-14 15:29:42 +0100

    First commit

---
+++ src/api/commands.py
@@ -0,0 +1,147 @@
+import click
+import string
+import random
+import names
+import uuid
+import requests
+import json
+from sqlalchemy import func
+from api.models import db, User, Product
+from datetime import datetime
+from flask_bcrypt import Bcrypt
+
+
+with open("data.json", "r") as f:
+    data = json.load(f)
+
+# --------------- Use this command to create Users and Products ---------------
+# -->            $ flask test-users 10 && flask test-products 50
+
+
+def setup_commands(app):
+    """Set up the test-users command for the Flask app."""
+
+    @app.cli.command("test-users")  # flask test-users $
+    @click.argument("count")  # argument of out command
+    def insert_test_user(count):
+        bcrypt = Bcrypt()
+
+        def generate_random_person():
+            response = requests.get("https://randomuser.me/api/")
+            if response.status_code == 200:
+                data = response.json()
+                name = data["results"][0]["name"]["first"]
+                surnames = data["results"][0]["name"]["last"]
+                email = data["results"][0]["email"]
+                password = data["results"][0]["login"]["password"]
+                location_city = data["results"][0]["location"]["city"]
+                location_state = data["results"][0]["location"]["state"]
+                location_country = data["results"][0]["location"]["country"]
+                location_postcode = data["results"][0]["location"]["postcode"]
+                dob_date = data["results"][0]["dob"]["date"]
+                dob_age = data["results"][0]["dob"]["age"]
+                registered_date = data["results"][0]["registered"]["date"]
+                phone = data["results"][0]["phone"]
+                picture_large = data["results"][0]["picture"]["large"]
+                picture_medium = data["results"][0]["picture"]["medium"]
+                picture_thumbnail = data["results"][0]["picture"]["thumbnail"]
+                return (
+                    name,
+                    surnames,
+                    email,
+                    password,
+                    location_city,
+                    location_state,
+                    location_country,
+                    location_postcode,
+                    dob_date,
+                    dob_age,
+                    registered_date,
+                    phone,
+                    picture_large,
+                    picture_medium,
+                    picture_thumbnail,
+                )
+            else:
+                print("Error al llamar a la API: ", response.status_code)
+                return ""
+
+        print("Creating test users...")
+        for x in range(1, int(count) + 1):
+            user = User()
+            (
+                name,
+                surnames,
+                email,
+                password,
+                location_city,
+                location_state,
+                location_country,
+                location_postcode,
+                dob_date,
+                dob_age,
+                registered_date,
+                phone,
+                picture_large,
+                picture_medium,
+                picture_thumbnail,
+            ) = generate_random_person()
+
+            user.name = name
+            user.surnames = surnames
+            user.email = email
+            user.is_admin = False
+            user.password = password
+            user.hash = bcrypt.generate_password_hash(password).decode("utf-8")
+            user.location_city = location_city
+            user.location_state = location_state
+            user.location_country = location_country
+            user.location_postcode = location_postcode
+            user.dob_age = dob_age
+            user.dob_date = dob_date
+            user.registered_date = registered_date
+            user.phone = phone
+            user.picture_large = picture_large
+            user.picture_medium = picture_medium
+            user.picture_thumbnail = picture_thumbnail
+
+            db.session.add(user)
+            db.session.commit()
+
+        print(f"Created {count} Users")
+
+    @app.cli.command("test-products")  # flask test-products $
+    @click.argument("count")  # argument of out command
+    def insert_test_product(count):
+        """Insert test data for the specified number of products."""
+
+        def random_price():
+            return random.randint(1, 9000)
+
+        status_options = ["active", "inactive", "completed"]
+
+        print("Creating test products...")
+        for x in range(1, int(count) + 1):
+            # Obtener un objeto de usuario aleatorio desde la base de datos
+            random_user = User.query.order_by(func.random()).first()
+            category = random.choice(list(data["categories"].keys()))
+            product_name = random.choice(data["categories"][category][0])
+            http_url = data["categories"][category][1]
+            word = random.choice(data["select_words"])
+
+            if category == http_url:
+                return http_url
+
+            product = Product()
+            product.name = f"{product_name} {word}"
+            product.description = f"A brief description of {product_name}"
+            product.category = category
+            product.price = random_price()
+            product.images = ":".join(http_url)
+            product.created_at_product = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            product.status_shooping = "active"
+            product.owner_id = random_user.id
+            db.session.add(product)
+            db.session.commit()
+
+        print(f"Created {count} products")
---
+++ src/api/models.py
@@ -0,0 +1,68 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey, Enum
+
+
+db = SQLAlchemy()
+
+
+class Product(db.Model):
+    __tablename__ = "Product"
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey("User.id"), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String, nullable=False)
+    category = db.Column(db.String, nullable=False)
+    price = db.Column(db.Numeric(precision=7, scale=2), nullable=False)
+    images = db.Column(db.String)
+    created_at_product = db.Column(db.DateTime, nullable=False)
+    status_shooping = db.Column(
+        db.Enum("active", "inactive", "reserved", name="status_shooping__"),
+        nullable=False,
+    )
+    owner = db.relationship("User", backref=db.backref("products", lazy=True))
+
+
+class User(db.Model):
+    __tablename__ = "User"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    surnames = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    is_admin = db.Column(db.Boolean)
+    password = db.Column(db.String(200), nullable=False)
+    hash = db.Column(db.Text)
+    location_city = db.Column(db.String(50))
+    location_state = db.Column(db.String(50))
+    location_country = db.Column(db.String(50))
+    location_postcode = db.Column(db.String(50))
+    dob_date = db.Column(db.String(100))
+    dob_age = db.Column(db.Integer, nullable=False)
+    registered_date = db.Column(db.String(100))
+    phone = db.Column(db.String(20))
+    picture_large = db.Column(db.String)
+    picture_medium = db.Column(db.String)
+    picture_thumbnail = db.Column(db.String)
+
+
+class ShoppingProduct(db.Model):
+    __tablename__ = "Shopping_Product"
+    id = db.Column(db.Integer, primary_key=True)
+    buyer_id = db.Column(db.Integer, ForeignKey("User.id"), nullable=False)
+    product_id = db.Column(db.Integer, ForeignKey("Product.id"), nullable=False)
+    status_shopping = db.Column(
+        db.Enum("active", "inactive", "completed", name="__status_shopping_enum"),
+        nullable=False,
+    )
+    created_at_shopping = db.Column(db.DateTime, nullable=False)
+    updated_at_shopping = db.Column(db.DateTime, nullable=False)
+    price = db.Column(db.Numeric, nullable=False)
+    status_paid = db.Column(
+        db.Enum("paid", "pending", "refunded", name="__status_paid_enum"), nullable=False
+    )
+    paid_at = db.Column(db.DateTime, nullable=False)
+    purchase_method = db.Column(db.String, nullable=False)
+    commission = db.Column(db.Numeric(6, 2), nullable=False)
+    buyer = db.relationship("User", backref=db.backref("shopping_products", lazy=True))
+    product = db.relationship(
+        "Product", backref=db.backref("shopping_products", lazy=True)
+    )
